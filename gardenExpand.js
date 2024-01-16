2a7549addd7061c17641e1bc19266a9b6a400d38
ashley773877 <mirandaashley16@gmail.com>
2024-01-16 09:55:58 -0500

    for function

--- gardenExpand.js
+++ gardenExpand.js
@@ -4,3 +4,8 @@ export function calculateGardenExpansion(initialPlants, weeks) {
     let cumulativeArea = 0;
     let currentPlants = initialPlants;
 
+    for (let week = 1; week <= weeks; week++) {
+        const areaRequired = currentPlants * minimumspaceperplant;
+        cumulativeArea += areaRequired;
+
+        currentPlants *= 2;
